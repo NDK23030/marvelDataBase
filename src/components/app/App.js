8eaa90759350a00c51dd4954fce6f195729fe9fe
Denis <ggmazanovdenis@gmail.com>
2023-05-09 01:16:26 +0300

    React Router

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,36 +1,33 @@
-// import { useState } from "react"
-import AppHeader from "../appHeader/AppHeader";
-// import RandomChar from "../randomChar/RandomChar";
-// import CharList from "../charList/CharList";
-// import CharInfo from "../charInfo/CharInfo";
-import ComicsList from "../comicsList/ComicsList";
-import SingleComic from "../singleComic/SingleComic";
-import AppBanner from "../appBanner/AppBanner";
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 
-// import decoration from '../../resources/img/vision.png';
+import AppHeader from "../appHeader/AppHeader";
+import {MainPage, ComicsPage} from "../pages";
+import SingleComic from "../singleComic/SingleComic";
+
+import decoration from '../../resources/img/vision.png';
 
 const App = () => {
-    // const [selectedChar, setChar] = useState(null)
-
-    // const onCharSelected = (id) => {
-    //     setChar(id);
-    // }
+    
 
     return (
-        <div className="app">
-            <AppHeader/>
-            <main>
-                <AppBanner/>
-                <SingleComic/>
-                {/* <ComicsList/> */}
-                {/* <RandomChar/>
-                <div className="char__content">
-                    <CharList onCharSelected={onCharSelected}/>
-                    <CharInfo charId={selectedChar}/>
-                </div>
-                <img className="bg-decoration" src={decoration} alt="vision"/> */}
-            </main>
-        </div>
+        <Router>
+            <div className="app">
+                <AppHeader/>
+                <main>
+                    {/* <AppBanner/>
+                    <SingleComic/> */}
+                    
+                    <Switch>
+                        <Route exact path="/">
+                            <MainPage/>
+                        </Route>
+                        <Route exact path="/comics">
+                            <ComicsPage/>
+                        </Route>
+                    </Switch>
+                </main>
+            </div>
+        </Router>
     )
 }
 
